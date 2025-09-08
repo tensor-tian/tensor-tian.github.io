c63b4f6bda9124d826a1b7d61a18aa7f5ae43383
tensor-tian <tensor.tian@gmail.com>
2025-09-08 16:21:37 +0800

    ignore dist

--- packages/copy-code/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Plugin } from "unified";
-import { Root } from "mdast";
-declare const remarkCopyCodeFile: Plugin<[], Root, Root>;
-export default remarkCopyCodeFile;
-//# sourceMappingURL=index.d.ts.map
